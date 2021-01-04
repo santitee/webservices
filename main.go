7cc7b6cc295abb025c306e830bcc6f5af6e11d48
santiteeragul <santitee@gmai.com>
2021-01-04 17:24:01 +0700

    Working with JSON

--- main.go
+++ main.go
@@ -1,21 +1,76 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productJSON := `[
+		{
+			"productId":1,
+			"manufacturer":"Johns-Jenkins",
+			"sku":"p5z343vds",
+			"upc":"939581000000",
+			"pricePerUnit":"497.45",
+			"quantityOnHand":3703,
+			"productName":"stricky note"
+		},
+		{
+			"productId":2,
+			"manufacturer":"Test",
+			"sku":"TestSku",
+			"upc":"TestUpc",
+			"pricePerUnit":"12.99",
+			"quantityOnHand":9500,
+			"productName":"ProductTest"
+		},
+		{
+			"productId":3,
+			"manufacturer":"Swaniawski, Bartoletti and Bruen",
+			"sku":"q0L657ys7",
+			"upc":"111730000000",
+			"pricePerUnit":"439.26",
+			"quantityOnHand":5905,
+			"productName":"lamp shade"
+		}
+	]`
+	err := json.Unmarshal([]byte(productJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func productsHandler(w http.ResponseWriter, r * http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJSON, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJSON)
+
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+
+	http.HandleFunc("/products", productsHandler)
+
 	http.ListenAndServe(":5000", nil)
 }
