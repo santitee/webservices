a9bec83ac7e41ebf4b60a917b273e2eb667b29f1
santiteeragul <santitee@gmai.com>
2021-01-04 22:47:51 +0700

    Working with Request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -54,6 +55,16 @@ func init() {
 	}
 }
 
+func getNextID() int {
+	highestID := -1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
+}
+
 func productsHandler(w http.ResponseWriter, r * http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -65,7 +76,28 @@ func productsHandler(w http.ResponseWriter, r * http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(productsJSON)
 
+	case http.MethodPost:
+		// Add a new product to the list
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newProduct.ProductID = getNextID()
+		productList = append(productList, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		return
 	}
+
 }
 
 func main() {
